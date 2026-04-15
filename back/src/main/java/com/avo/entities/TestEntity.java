530f2c0be08c2689aea6f7295cda1e3dda8be149
adam-115 <adam.laftimi@gmail.com>
2026-04-15 17:04:34 +0100

    add hiberrnae and liquibase
    add hibernate and liquibase

---
+++ back/src/main/java/com/avo/entities/TestEntity.java
@@ -0,0 +1,41 @@
+package com.avo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "test_entities")
+public class TestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    public TestEntity() {
+    }
+
+    public TestEntity(String message) {
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
