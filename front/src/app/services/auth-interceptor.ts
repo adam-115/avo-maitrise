5e66b633f01c95f4ceb40e3755ed389a38a29c3a
adam-115 <adam.laftimi@gmail.com>
2026-06-22 18:48:38 +0100

    add redirect to home page if session is closed
    add redirect to home page if session is closed

--- front/src/app/services/auth-interceptor.ts
+++ front/src/app/services/auth-interceptor.ts
@@ -1,18 +1,28 @@
 import { inject } from '@angular/core';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { KeycloakService } from './keycloak.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const keycloakService = inject(KeycloakService);
   const token = keycloakService.getToken();
 
+  let authReq = req;
   if (token) {
-    const cloned = req.clone({
+    authReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(cloned);
   }
-  return next(req);
+
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        keycloakService.logout();
+      }
+      return throwError(() => error);
+    })
+  );
 };
